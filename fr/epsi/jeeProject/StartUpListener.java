e39b5cd31347cf3aa2469aa600f95b722edf1c67
ALeroy2 <39801664+ALeroy2@users.noreply.github.com>
2019-04-19 09:29:04 +0200

    Ajout dans le Listener, ajout de Premier.java, PremierMBean.java

--- fr/epsi/jeeProject/StartUpListener.java
+++ fr/epsi/jeeProject/StartUpListener.java
@@ -1,5 +1,10 @@
-package fr.epsi.jeeProject.listener;
+package fr.epsi.jeeProject;
 
+import fr.epsi.jeeProject.jmx.Premier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.management.*;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -7,45 +12,56 @@ import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import javax.servlet.http.HttpSessionBindingEvent;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.lang.management.ManagementFactory;
 
 @WebListener()
-public class Listener implements ServletContextListener,
+public class StartUpListener implements ServletContextListener,
         HttpSessionListener, HttpSessionAttributeListener {
 
+    private static final Logger logger = LogManager.getLogger(StartUpListener.class);
 
     // Public constructor is required by servlet spec
-    public Listener() {
+    public StartUpListener() {
+
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = null;
+
+        try {
+            name = new ObjectName("fr.epsi.jmx:type=PremierMBean");
+            Premier mbean = new Premier();
+
+            mbs.registerMBean(mbean, name);
+
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (InstanceAlreadyExistsException e) {
+            e.printStackTrace();
+        } catch (MBeanRegistrationException e) {
+            e.printStackTrace();
+        } catch (NotCompliantMBeanException e) {
+            e.printStackTrace();
+        }
     }
 
     // -------------------------------------------------------
     // ServletContextListener implementation
     // -------------------------------------------------------
     public void contextInitialized(ServletContextEvent sce) {
-
-        System.out.println("Démarrage");
-
-        try{
-            Class.forName("org.hsqldb.jdbcDriver");
-            Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:9003","SA","");
-            System.out.println("connexion ok sur localhost");
-            con.close();
-        }catch (ClassNotFoundException e){
-            System.out.println("Driver not avaible "+ e);
-        }catch (SQLException e){
-            System.out.println("Error while connecting to DB"+ e);
-        }
+      /* This method is called when the servlet context is
+         initialized(when the Web application is deployed).
+         You can initialize servlet context related data here.
+      */
+        logger.debug("Lancement de l'application");
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-      /* This method is invoked when the Servlet Context 
-         (the Web application) is undeployed or 
+      /* This method is invoked when the Servlet Context
+         (the Web application) is undeployed or
          Application Server shuts down.
       */
+        logger.debug("Fermeture de l'application");
     }
 
     // -------------------------------------------------------
@@ -53,10 +69,12 @@ public class Listener implements ServletContextListener,
     // -------------------------------------------------------
     public void sessionCreated(HttpSessionEvent se) {
         /* Session is created. */
+        logger.debug("Ouverture de la session");
     }
 
     public void sessionDestroyed(HttpSessionEvent se) {
         /* Session is destroyed. */
+        logger.debug("Fermeture de la session");
     }
 
     // -------------------------------------------------------
@@ -64,7 +82,7 @@ public class Listener implements ServletContextListener,
     // -------------------------------------------------------
 
     public void attributeAdded(HttpSessionBindingEvent sbe) {
-      /* This method is called when an attribute 
+      /* This method is called when an attribute
          is added to a session.
       */
     }
